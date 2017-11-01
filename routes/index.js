8e93c4ad97bc26e0348354054e9f7f1a9467494b
unknown <richwbates@gmail.com>
2017-11-01 09:52:18 -0400

    obtained burgers from database and assigned them to 1 of 2 arrays based on if the burger was ate

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,22 @@ var Model 	= require('../models/index.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	User.burger.findAll().then(burgers => {
-  		console.log(burgers)
+	var devouredBurgers  = [];
+	var availableBurgers = [];
+	var thisBurger;
+	var burgerName;
+
+	Model.burger.findAll().then(burgers => {
+  		// console.log(burgers[0].dataValues)
+  		for(var burger in burgers){
+  			thisBurger = burgers[burger].dataValues
+  			burgerName = thisBurger.burger_name
+  			if (thisBurger.devoured) {
+  				devouredBurgers.push(burgerName);
+  			}else{
+  				availableBurgers.push(burgerName);
+  			}
+  		}
   	});
 });
 
