8723e7005926133d97b3cb211e29fb539d052d10
unknown <richwbates@gmail.com>
2017-11-01 23:53:48 -0400

    added update code to set devouer to true, moved find to function, added decodeuri when getting path url

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,28 @@ var Model 	= require('../models/index.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	getBurgers(res);
+});
+
+router.post('/api/eat/:burgerate', function(req, res, next) {
+	var burgerAte = req['url'].replace('/api/eat/','');
+	burgerAte = decodeURI(burgerAte);
+	console.log(burgerAte);
+	Model.burger
+		.update(
+			{
+				devoured: true
+			},
+			{
+				where: {burger_name: burgerAte}
+			}
+		)
+		.then(() => {
+			getBurgers(res);
+		});
+});
+
+function getBurgers(res){
 	var devouredBurgers  = [];
 	var availableBurgers = [];
 	var thisBurger;
@@ -27,6 +49,6 @@ router.get('/', function(req, res, next) {
   			
   			res.render('index', {devouredBurgers : devouredBurgers, availableBurgers : availableBurgers});
   		});
-});
+}
 
 module.exports = router;
